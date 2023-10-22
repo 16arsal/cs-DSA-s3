7aeb54cff9e86538db17303f7b982479a540548a
16arsal <muhammadarsal28@gmail.com>
2023-10-22 22:25:17 +0500

    ez

--- practice/singlelinkedlistpractice.cpp
+++ practice/singlelinkedlistpractice.cpp
@@ -1,66 +1,115 @@
 #include <iostream>
 using namespace std;
+#include <iostream>
 
-struct Node 
-{
+struct Node {
     int data;
     Node* next;
 };
 
-class LinkedList 
-{ 
-public:
-
+class LinkedList {
+private:
     Node* head;
 
-    LinkedList() 
-    {
+public:
+    LinkedList() {
         head = nullptr; // Initialize an empty list
     }
 
-    void insertAtBeginning(int value) 
-    {
-    Node* newNode = new Node;
-    newNode->data = value;
-    newNode->next = head;
-    head = newNode;
-    }
-
-    void insertAtEnd(int value) {
-    Node* newNode = new Node;
-    newNode->data = value;
-    newNode->next = nullptr;
-
-    if (head == nullptr) {
+    // Insertion at the beginning
+    void insertAtBeginning(int value) {
+        Node* newNode = new Node;
+        newNode->data = value;
+        newNode->next = head;
         head = newNode;
-        return;
     }
 
-    Node* current = head;
-    while (current->next != nullptr) {
-        current = current->next;
+    // Insertion at the end
+    void insertAtEnd(int value) {
+        Node* newNode = new Node;
+        newNode->data = value;
+        newNode->next = nullptr;
+
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+
+        Node* current = head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+
+        current->next = newNode;
     }
 
-    current->next = newNode;
-}
+    // Display the linked list
     void display() {
-    Node* current = head;
-    while (current != nullptr) {
-        cout << current->data << " -> ";
-        current = current->next;
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->data << " -> ";
+            current = current->next;
+        }
+        cout << "nullptr" << endl;
     }
-    cout << "nullptr" << endl;
-}
 
+    // Search for a value in the linked list
+    bool search(int value) {
+        Node* current = head;
+        while (current != nullptr) {
+            if (current->data == value) {
+                return true;
+            }
+            current = current->next;
+        }
+        return false;
+    }
 
+    // Delete the first occurrence of a value
+    void deleteNode(int value) {
+        if (head == nullptr) {
+            return; // Nothing to delete, the list is empty
+        }
+
+        if (head->data == value) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return;
+        }
+
+        Node* current = head;
+        while (current->next != nullptr) {
+            if (current->next->data == value) {
+                Node* temp = current->next;
+                current->next = current->next->next;
+                delete temp;
+                return;
+            }
+            current = current->next;
+        }
+    }
 };
 
+int main() {
+    LinkedList myList;
 
+    myList.insertAtBeginning(5);
+    myList.insertAtBeginning(10);
+    myList.insertAtEnd(15);
 
+    myList.display();
 
+    if (myList.search(10)) {
+        cout << "Found 10 in the list." << endl;
+    } else {
+        cout << "10 not found in the list." << endl;
+    }
 
-int main()
+    myList.deleteNode(10);
+    myList.display();
 
-{
-    cout<<"ez";
+    return 0;
 }
+
+
