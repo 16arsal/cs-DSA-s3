7312b5bc8c60e65fd5b8790a74950ac10bf66bdb
16arsal <muhammadarsal28@gmail.com>
2023-10-30 12:54:22 +0500

    edit

--- DSA Codes/insertionsort.cpp
+++ DSA Codes/insertionsort.cpp
@@ -9,7 +9,8 @@ void insertionSort(int arr[], int size) {
 
         // Move elements of arr[0..i-1] that are greater than key
         // to one position ahead of their current position
-        while (j >= 0 && arr[j] > key) {
+        while (j >= 0 && arr[j] > key)
+        {
             arr[j + 1] = arr[j];
             j--;
         }
